21b0db621c9c53801ca210ae774394a298559f17
thuita faith <thuitamuthoni15@gmail.com>
2018-02-26 10:05:27 +0300

    Add folder structue

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
 //Business logic
-var pingPong = function(number) {
-  for(var a=0; a<=number.length; a+=1) {
-    if (number % 15=== 0){
-      return true;
-    } else if (number % 5=== 0) {
-      return true;
-    } else if(number % 3=== 0) {
-      return true;
+function pingPong(number) {
+  var myArray = [];
+  for(var a=0; a<=number; a++) {
+    if (a % 15=== 0){
+      myArray.push("Ping Pong!");
+    } else if (a % 5=== 0) {
+      myArray.push("Pong!");
+    } else if(a % 3=== 0) {
+      myArray.push("Ping!");
     }
-
+      else {myArray.push (a);}
     };
+    return myArray;
   };
 
 //User Interface logic
 $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
       event.preventDefault();
+      $(".col-result").empty();
       var number = parseInt($("input#number").val());
-      var result = pingPong(number);
-      $("#result").text(result);
+      var result = pingPong(number)+ '';
+      var num = result.split(",").join("<br>");
+      $("div.result1").addClass("col-result").append("<li class='col-result'"+ num + "</li>");
     });
   });
