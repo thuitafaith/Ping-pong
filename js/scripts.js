31a3eb33505f3a838a3f7b5b353f9ace8e074009
thuita faith <thuitamuthoni15@gmail.com>
2018-02-26 07:59:53 +0300

    commit all new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
 //Business logic
+var pingPong = function(number) {
+  for(var a=0; a<=number.length; a+=1) {
+    if (number % 15=== 0){
+      return true;
+    } else if (number % 5=== 0) {
+      return true;
+    } else if(number % 3=== 0) {
+      return true;
+    }
 
-
-
-
-
-
-
-
-
-
-
+    };
+  };
 
 //User Interface logic
 $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
       event.preventDefault();
-      var year = parseInt($("input#number").val());
+      var number = parseInt($("input#number").val());
       var result = pingPong(number);
       $("#result").text(result);
     });
